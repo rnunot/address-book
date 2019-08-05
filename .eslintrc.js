bd1cd539320e1456a51f89f837e300eaa7b37ea8
Ricardo Tavares <rnunot@gmail.com>
2019-08-05 17:17:50 +0100

    Add missing files, add prettier commands

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    jest: true,
   },
   extends: [
     'plugin:vue/recommended',
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
-import '@/registerServiceWorker';
+import '@/misc/registerServiceWorker';
+import '@/misc/handleNetworkStatus';
+import '@/misc/handleAppleInstallPrompt';
 
 Vue.config.productionTip = false;
 
