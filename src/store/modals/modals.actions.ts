9aa80fdf12cc94ccbfb7500016aaa4fe507cc718
Ricardo Tavares <rnunot@gmail.com>
2019-08-19 18:13:44 +0100

    Close modal on back button

--- src/store/modals/modals.actions.ts
+++ src/store/modals/modals.actions.ts
@@ -1,12 +1,42 @@
 import { ActionTree } from 'vuex';
 import { RootState } from '@/store/types';
 import { ModalsState } from '@/store/modals/types';
+import router from '@/router';
+
+const contactsHistoryKey = 'contacts-model-state';
 
 export default {
-  showCreateContactModal({ state, commit }) {
+  showCreateContactModal({ commit, dispatch }) {
     commit('setIsCreateContactModalOpen', true);
+    window.history.replaceState(
+      { ...(history.state || {}), historyKey: contactsHistoryKey },
+      'key',
+    );
+    window.history.pushState({}, 'key2');
   },
   hideCreateContactModal({ state, commit }) {
     commit('setIsCreateContactModalOpen', false);
+    router.go(-1);
+  },
+
+  init: {
+    root: true,
+    handler({ state, commit }) {
+      window.addEventListener('popstate', event => {
+        const mutationMap = {
+          contactsHistoryKey: 'setIsCreateContactModalOpen',
+        };
+
+        if (
+          event.state &&
+          mutationMap[event.state.historyKey as keyof typeof mutationMap]
+        ) {
+          commit(
+            mutationMap[event.state.historyKey as keyof typeof mutationMap],
+            false,
+          );
+        }
+      });
+    },
   },
 } as ActionTree<ModalsState, RootState>;
