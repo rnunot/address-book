4692be214f6623e2467869a339ec41c1d64985d6
Ricardo Tavares <rnunot@gmail.com>
2019-08-19 16:30:08 +0100

    Fix contacts primary key

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { AppDB } from '@/db/types';
 
 export default openDB<AppDB>('address-book-db', 1, {
   upgrade(db) {
-    db.createObjectStore('contacts', { keyPath: 'phone' });
+    db.createObjectStore('contacts', { keyPath: 'name' });
     db.createObjectStore('groups', { keyPath: 'id' });
   },
 });
