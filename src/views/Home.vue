08fdd95dbc1945e07060d4345bc95c44d62b2162
Ricardo Tavares <rnunot@gmail.com>
2019-08-20 10:40:42 +0100

    Add floating create contact button to mobile screens

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,34 @@
   <div class="pt-5">
     <contacts-table />
     <create-contact-modal />
+
+    <button
+      type="button"
+      class="app__create-contact-button fixed md:hidden"
+      @click="showCreateContactModal"
+    >
+      <font-awesome-icon icon="plus" class="mx-2" />
+      Create contact
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { mapActions } from 'vuex';
 import ContactsTable from '@/components/contacts/ContactsTable.vue';
 import CreateContactModal from '@/components/contacts/CreateContactModal.vue';
 
 export default Vue.extend({
   name: 'Home',
   components: { ContactsTable, CreateContactModal },
+  methods: mapActions('modals', ['showCreateContactModal']),
 });
 </script>
+
+<style lang="scss" scoped>
+.app__create-contact-button {
+  bottom: theme('height.5');
+  right: theme('width.5');
+}
+</style>
