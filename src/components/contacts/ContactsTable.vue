dc71a0ecc0e46c1d212c1759625e66308b792953
Ricardo Tavares <rnunot@gmail.com>
2019-08-23 20:47:17 +0100

    Adjust table

--- src/components/contacts/ContactsTable.vue
+++ src/components/contacts/ContactsTable.vue
@@ -3,30 +3,46 @@
     <table class="w-full">
       <thead>
         <tr class="border-b">
-          <th class="md:w-1/3">Name</th>
-          <th class="hidden md:table-cell md:w-1/3">Phone</th>
-          <th class="hidden md:table-cell md:w-1/3">Group</th>
+          <th class="md:w-1/2 lg:w-1/3">Name</th>
+          <th class="hidden md:table-cell md:w-1/2 lg:w-1/3">Phone</th>
+          <th class="hidden lg:table-cell lg:w-1/3">Group</th>
         </tr>
       </thead>
       <transition-group name="table-fade" tag="tbody">
         <tr
           v-for="contact in contacts"
-          :key="contact.name"
+          :key="contact.id"
           class="hover:bg-purple-100 cursor-pointer"
           @click="showDetails(contact)"
         >
-          <td>
-            <app-img-loader
-              :src="contact.pictureUrl"
-              :placeholder-src="contactImgPlaceholder"
-              alt=""
-              class="h-12 w-12 rounded-full object-cover inline mr-2"
-            />
-            {{ contact.name }}
+          <td class="truncate">
+            <div class="table table-fixed w-full truncate">
+              <div class="flex items-center">
+                <app-img-loader
+                  :src="contact.pictureUrl"
+                  :placeholder-src="contactImgPlaceholder"
+                  alt=""
+                  class="h-12 w-12 rounded-full object-cover inline mr-2"
+                />
+                <div class="truncate">
+                  {{ contact.name }}
+                </div>
+              </div>
+            </div>
           </td>
-          <td class="hidden md:table-cell">{{ contact.phone }}</td>
           <td class="hidden md:table-cell">
-            {{ contact.groupName }}
+            <div class="table table-fixed w-full truncate">
+              <div class="truncate">
+                {{ contact.phone }}
+              </div>
+            </div>
+          </td>
+          <td class="truncate hidden lg:table-cell">
+            <div class="table table-fixed w-full truncate">
+              <div class="truncate">
+                {{ contact.groupName }}
+              </div>
+            </div>
           </td>
         </tr>
       </transition-group>
